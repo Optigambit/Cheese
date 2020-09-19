ff529e2f5c46e1a4847e02162e0b9a38cd40207a
PvtPuddles <stephev1@umbc.edu>
2020-09-19 13:32:10 -0400

    Created Mouse Pathfinding

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mouse : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = transform.GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
+        agent.SetDestination(player.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
