91c5f1e7a3565fd81593816105f48905f6557a14
PvtPuddles <stephev1@umbc.edu>
2020-09-20 14:44:20 -0400

    Revert "added mouse trap"
    This reverts commit b66de3cd626c853129b5b8cc5039f6d477d8e786.

--- Assets/Scripts/Mouse.cs
+++ Assets/Scripts/Mouse.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,7 +7,6 @@ public class Mouse : MonoBehaviour
 {
     NavMeshAgent agent;
     GameObject player;
-    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,19 +18,6 @@ public class Mouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timer <= 0) {
-            agent.SetDestination(player.transform.position);
-        }
-        else {
-            timer -= Time.deltaTime;
-        }
-    }
-
-    void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag.Equals("trap")) {
-            GameObject.Find(collision.gameObject.name).GetComponent<mouseTrap>().hit();
-            timer = 5;
-            agent.SetDestination(transform.position);
-        }
+        agent.SetDestination(player.transform.position);
     }
 }
